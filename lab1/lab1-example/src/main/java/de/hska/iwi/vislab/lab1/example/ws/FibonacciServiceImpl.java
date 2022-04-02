3831ad3f9e97b58f4c118764d5711f0b290d533b
Marius Butz <mbpictures13@gmail.com>
2022-04-02 14:59:19 +0200

    lab1: add fibonacci service

---
+++ lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/ws/FibonacciServiceImpl.java
@@ -0,0 +1,17 @@
+package de.hska.iwi.vislab.lab1.example.ws;
+
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "de.hska.iwi.vislab.lab1.example.ws.FibonacciService")
+public class FibonacciServiceImpl implements FibonacciService {
+    @Override
+    public int getFibonacci(int n) {
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+}
